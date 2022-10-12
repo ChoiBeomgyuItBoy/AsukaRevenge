0f5c677d4ff2abb4e12343aed6b991fbc496b043
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-12 18:31:38 -0500

    Main Logic Finished

--- Assets/Player/PlayerTestState.cs
+++ Assets/Player/PlayerTestState.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float time = 5f;
+    private float time = 0f;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){}
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
         Debug.Log(time);
 
-        time -= deltaTime;
-
-        if(time <= 0)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
+        time += deltaTime;
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
