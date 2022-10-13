b79fc9e70d586cb2e3994d901682aa831b30c0ab
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-13 05:00:05 -0500

    Refactor Movement

---
+++ Assets/Player/PlayerFreeLookState.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFreeLookState : PlayerBaseState
+{
+    private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed");
+
+    private const float AnimatorDampTime = 0.1f;
+
+    public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine){}
+
+    public override void Enter()
+    {
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Vector3 movement = CalculateMovement();
+
+        stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if (stateMachine.InputReader.MovementValue == Vector2.zero)
+        {
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0f, AnimatorDampTime, Time.deltaTime);
+            return;
+        }
+
+        stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1f, AnimatorDampTime, Time.deltaTime);
+        FaceMovementDirection(movement, deltaTime);
+    }
+
+    public override void Exit()
+    {
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right= stateMachine.MainCameraTransform.right;
+
+        forward.y = 0f; 
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y 
+                + right * stateMachine.InputReader.MovementValue.x;
+    }
+
+    private void FaceMovementDirection(Vector3 movement, float deltaTime)
+    {
+        stateMachine.transform.rotation = Quaternion.Lerp(
+            stateMachine.transform.rotation,
+            Quaternion.LookRotation(movement),
+            deltaTime * stateMachine.RotationDamping
+        );
+    }
+}
--- Assets/Player/PlayerStateMachine.cs
+++ Assets/Player/PlayerStateMachine.cs
@@ -7,7 +7,9 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private set; }
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public Animator Animator { get; private set; }
+
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+    [field: SerializeField] public float RotationDamping { get; private set; }
 
      public Transform MainCameraTransform { get; private set; }
 
@@ -15,6 +17,6 @@ public class PlayerStateMachine : StateMachine
     {
         MainCameraTransform = Camera.main.transform;
 
-        SwitchState(new PlayerTestState(this));
+        SwitchState(new PlayerFreeLookState(this));
     }
 }
