5aee838a990825d51ec0b85b2e698e7d1c290e41
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-16 13:45:51 -0500

    Added Attack Combo, Attack Force and Attack Animations

--- Assets/ForceReceiving/ForceReceiver.cs
+++ Assets/ForceReceiving/ForceReceiver.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class ForceReceiver : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
-    private float verticalVelocity;
+    [SerializeField] private float drag = 0.3f;
 
-    public Vector3 Movement => Vector3.up * verticalVelocity;
+    private Vector3 dampingVelocity;
+    private Vector3 impact;
+    private float verticalVelocity;
+    
+    public Vector3 Movement => impact + Vector3.up * verticalVelocity;
 
     private void Update()
     {
@@ -19,5 +23,12 @@ public class ForceReceiver : MonoBehaviour
         {
             verticalVelocity += Physics.gravity.y * Time.deltaTime;
         }
+
+        impact = Vector3.SmoothDamp(impact, Vector3.zero, ref dampingVelocity, drag);
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        impact += force;
     }
 }
